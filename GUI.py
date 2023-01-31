7611b109f705313e0ed53271f4efe282b4625393
rodrigoparri <rodrigoparri@gmail.com>
2023-01-31 20:07:34 +0100

    developing a system to make automatic forms

--- GUI.py
+++ GUI.py
@@ -1,25 +1,58 @@
 from PySide6 import QtWidgets as QtW
-from PySide6 import QtCore as QtC
+
+
 
 class MainWindow(QtW.QMainWindow):
 
-    def  __init__(self):
-        super().__init__()
+    form_fields = {
+        1:("b (mm): ", "200"),
+        0:("h (mm): ", "300"),
+        2:("fck (MPa): ", "25"),
+        3:("γc: ", "1.5"),
+        4:("fyk (MPa): ", "500"),
+        5:("γs: ", "1.15"),
+        6:("c (mm): ", "30"),
+        7:("Md (mkN): ", "300"),
+        8:("", ""),
+        9:("", "")
+
+    }
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
         self.setWindowTitle("Concrete Calculator")
+        layout = QtW.QFormLayout()
 
-        self.widgets(self)
+        for i in self.form_fields:
+            self.addFormRow(layout, self.form_fields[i][0], self.form_fields[i][1])
 
-    @classmethod
-    def widgets(cls,self):
-        cls.h = Entry(self, "h")
+
+        central_widget = QtW.QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def addFormRow(self, layout, label_tx, entry_tx):
+
+        label = Label(label_tx)
+        entry = Entry(self, entry_tx)
+        layout.addRow(label, entry)
 
 
 class Entry(QtW.QLineEdit):
-    
-    def __init__(self, parent, text):
-        super().__init__(parent, text)
 
-        self.setPlaceholderText(text)
+    def __init__(self, parent, deftext):
+        super(Entry, self).__init__(parent, deftext)
+
+        self.setPlaceholderText(deftext)
+
+
+class Label(QtW.QLabel):
+
+    def __init__(self, text):
+        super(Label, self).__init__(text)
+
+
+
 
 if __name__ == "__main__":
     app = QtW.QApplication([])
