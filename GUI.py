2c11211a81474d48bf422478ecc0dfd248aefadf
rodrigoparri <rodrigoparri@gmail.com>
2023-01-31 21:21:07 +0100

    need a way to implement a Qpainter

--- GUI.py
+++ GUI.py
@@ -5,8 +5,8 @@ from PySide6 import QtWidgets as QtW
 class MainWindow(QtW.QMainWindow):
 
     form_fields = {
-        1:("b (mm): ", "200"),
-        0:("h (mm): ", "300"),
+        0:("b (mm): ", "200"),
+        1:("h (mm): ", "300"),
         2:("fck (MPa): ", "25"),
         3:("γc: ", "1.5"),
         4:("fyk (MPa): ", "500"),
@@ -15,21 +15,32 @@ class MainWindow(QtW.QMainWindow):
         7:("Md (mkN): ", "300"),
         8:("", ""),
         9:("", "")
-
     }
     def __init__(self):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("Concrete Calculator")
-        layout = QtW.QFormLayout()
 
+        #------------------- LAYOUTS----------------------
+        boxh_layout = QtW.QHBoxLayout()  # horizontal layout for form and canvas
+        form_layout = QtW.QFormLayout()  # form layout for data input
+
+        #--------------------PARENT WIDGET---------------
+        central_widget = QtW.QWidget()  # parent widget
+        central_widget.setLayout(boxh_layout)  # set widget layout
+        self.setCentralWidget(central_widget)  # set widget as central
+
+        #-------------------CHILD WIDGETS------------------
+        left_widget = QtW.QWidget()
+        left_widget.setLayout(form_layout)
+        boxh_layout.addWidget(left_widget)
+
+        right_widget = QtW.QWidget()
+        boxh_layout.addWidget(right_widget)
+
+        #-----------------FORM WIDGETS---------------------
         for i in self.form_fields:
-            self.addFormRow(layout, self.form_fields[i][0], self.form_fields[i][1])
-
-
-        central_widget = QtW.QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+            self.addFormRow(form_layout, self.form_fields[i][0], self.form_fields[i][1])
 
     def addFormRow(self, layout, label_tx, entry_tx):
 
