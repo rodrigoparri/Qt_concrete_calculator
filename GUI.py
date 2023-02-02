71090f9c731fc317a66754b3903f055d610a90fa
rodrigoparri <rodrigoparri@gmail.com>
2023-02-02 01:17:42 +0100

    grid layout works perfectly fine. lets try some group boxing

--- GUI.py
+++ GUI.py
@@ -4,8 +4,70 @@ from PySide6 import  QtGui as QtG
 
 class RenderArea(QtW.QWidget):
     
-    def __init__(self):
-        super(RenderArea, self).__init__()
+    def __init__(self, parent = None):
+        super(RenderArea, self).__init__(parent)
+
+        self.setMinimumSize(150, 200)
+        # self.setMaximumSize(400, 600)
+        self.setBackgroundRole(QtG.QPalette.Base)
+        self.setAutoFillBackground(True)
+
+        self.pen = QtG.QPen()
+        self.antialiased = False
+
+        self.input_values = {
+            "b": 100,
+            "h": 150
+        }
+
+    def set_pen(self, pen):
+        self.pen = pen
+        self.update()
+
+    def set_antialising(self, antialiased):
+        self.antialiased = antialiased
+        self.update()
+
+    def set_input_values(self, values: dict):
+        self.input_values = values
+
+
+    # def get_b(self):
+    #     b = self.input_values["b"]
+    #     return b
+    #
+    # def get_h(self):
+    #     h = self.input_values["h"]
+    #     return h
+    #
+    # def get_As1(self):
+    #     As1 = self.input_values["As1"]
+    #     return As1
+    #
+    # def get_As2(self):
+    #     As2 = self.input_values["As2"]
+    #     return As2
+
+    def paintEvent(self, event) -> None:  # doesn´t return anything
+
+        with QtG.QPainter(self) as painter:
+            painter.setPen(self.pen)
+            if self.antialiased:
+                painter.setRenderHint(QtG.QPainter.Antialiasing)
+
+            width = self.width()
+            height = self.height()
+
+            b = self.input_values.get("b", 0)
+            h = self.input_values.get("h", 0)
+
+            x = (width - b) / 2
+            y = (height - h) / 2
+
+            As1 = self.input_values.get("As1", 0)
+            As2 = self.input_values.get("As2", 0)
+
+            painter.drawRect(x, y, b, h)
 
 class MainWindow(QtW.QMainWindow):
 
@@ -22,7 +84,7 @@ class MainWindow(QtW.QMainWindow):
         self.b_entry = QtW.QLineEdit(self)
         self.b_entry.setPlaceholderText("200")
         self.h_entry = QtW.QLineEdit(self)
-        self.h_entry.setPlaceholderText("300")
+        self.h_entry.setPlaceholderText("100")
         self.fck_entry = QtW.QLineEdit(self)
         self.fck_entry.setPlaceholderText("25")
         self.yc_entry = QtW.QLineEdit(self)
@@ -53,6 +115,10 @@ class MainWindow(QtW.QMainWindow):
         self.Md_label = QtW.QLabel("Md (mkN): ")
         self.Md_label.setBuddy(self.Md_entry)
 
+        #--------------------RENDERAREA------------------
+        self.render_area = RenderArea()
+        self.render_area.set_antialising(True)
+
         # ------------------- LAYOUT----------------------
         layout = QtW.QGridLayout()
         layout.setRowStretch(0, 10)
@@ -60,7 +126,7 @@ class MainWindow(QtW.QMainWindow):
         layout.addWidget(self.b_label, 0, 0)
         layout.addWidget(self.b_entry, 0, 1)
         layout.addWidget(self.h_label, 1, 0)
-        layout.addWidget(self.h_label, 1, 1)
+        layout.addWidget(self.h_entry, 1, 1)
         layout.addWidget(self.fck_label, 2, 0)
         layout.addWidget(self.fck_entry, 2, 1)
         layout.addWidget(self.yc_label, 3, 0)
@@ -73,6 +139,8 @@ class MainWindow(QtW.QMainWindow):
         layout.addWidget(self.c_entry, 6, 1)
         layout.addWidget(self.Md_label, 7, 0)
         layout.addWidget(self.Md_entry, 7, 1)
+
+        layout.addWidget(self.render_area, 0, 2, 8, 1)
 
         central_widget.setLayout(layout)
 
