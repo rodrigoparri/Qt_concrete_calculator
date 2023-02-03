ccab6739bc18114180b19fadd9bd5658308a84cf
rodrigoparri <rodrigoparri@gmail.com>
2023-02-03 22:32:49 +0100

    conections between GUI and calculations where presented. now the beam changes shape. carefull with larges dimensions. some scaling sistem is required so the beams get drawn properly

--- GUI.py
+++ GUI.py
@@ -1,6 +1,8 @@
 from PySide6 import QtWidgets as QtW
 from PySide6 import QtGui as QtG
 
+from concrete_cross_sections.rect_beams import RectBeam
+
 
 class RenderArea(QtW.QWidget):
     
@@ -58,7 +60,7 @@ class MainWindow(QtW.QMainWindow):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("Concrete Calculator")
-        self.setMaximumSize(400,300)
+        self.setFixedSize(400,300)
         #---------------------MAIN WIDGET------------------
         central_widget = QtW.QWidget()
         self.setCentralWidget(central_widget)
@@ -68,9 +70,9 @@ class MainWindow(QtW.QMainWindow):
         self.b_entry = QtW.QLineEdit(self)
         self.b_entry.setPlaceholderText("200")
         self.h_entry = QtW.QLineEdit(self)
-        self.h_entry.setPlaceholderText("100")
-        self.c_entry = QtW.QLineEdit(self)
-        self.c_entry.setPlaceholderText("30")
+        self.h_entry.setPlaceholderText("300")
+        self.expo_entry = QtW.QLineEdit(self)
+        self.expo_entry.setPlaceholderText("XC1")
         self.fck_entry = QtW.QLineEdit(self)
         self.fck_entry.setPlaceholderText("25")
         self.yc_entry = QtW.QLineEdit(self)
@@ -88,8 +90,8 @@ class MainWindow(QtW.QMainWindow):
         self.b_label.setBuddy(self.b_entry)
         self.h_label = QtW.QLabel("h (mm): ")
         self.h_label.setBuddy(self.h_entry)
-        self.c_label = QtW.QLabel("c (mm): ")
-        self.c_label.setBuddy(self.c_entry)
+        self.expo_label = QtW.QLabel("Exposure class: ")
+        self.expo_label.setBuddy(self.expo_entry)
         self.fck_label = QtW.QLabel("fck (MPa): ")
         self.fck_label.setBuddy(self.fck_entry)
         self.yc_label = QtW.QLabel("γc: ")
@@ -105,10 +107,18 @@ class MainWindow(QtW.QMainWindow):
 
         geo_separator = QtW.QFrame()
         geo_separator.setFrameShape(QtW.QFrame.HLine)
+        geo_separator.setLineWidth(1)
+        geo_separator.setStyleSheet("color: lightgrey;")
+        
         mat_separator = QtW.QFrame()
         mat_separator.setFrameShape(QtW.QFrame.HLine)
+        mat_separator.setLineWidth(1)
+        mat_separator.setStyleSheet("color: lightgrey;")
+
         adv_separator = QtW.QFrame()
         adv_separator.setFrameShape(QtW.QFrame.HLine)
+        adv_separator.setLineWidth(1)
+        adv_separator.setStyleSheet("color: lightgrey;")
 
         #-------------------BUTTONS----------------------
         self.calc_button = QtW.QPushButton("Calculate")
@@ -118,16 +128,18 @@ class MainWindow(QtW.QMainWindow):
         self.render_area = RenderArea()
         self.render_area.set_antialising(True)
 
+        #--------------------BUTTON CONNECTIONS-----------
+        self.calc_button.clicked.connect(self.calculate)
         # ------------------- LAYOUT----------------------
         layout = QtW.QGridLayout()
-        layout.setRowStretch(0, 10)
+        layout.setRowStretch(0, 20)
         layout.setColumnStretch(0, 2)
         layout.addWidget(self.b_label, 0, 0)
         layout.addWidget(self.b_entry, 0, 1)
         layout.addWidget(self.h_label, 1, 0)
         layout.addWidget(self.h_entry, 1, 1)
-        layout.addWidget(self.c_label, 2, 0)
-        layout.addWidget(self.c_entry, 2, 1)
+        layout.addWidget(self.expo_label, 2, 0)
+        layout.addWidget(self.expo_entry, 2, 1)
         layout.addWidget(geo_separator, 3, 0, 1, 2)
         layout.addWidget(self.fck_label, 4, 0)
         layout.addWidget(self.fck_entry, 4, 1)
@@ -149,6 +161,25 @@ class MainWindow(QtW.QMainWindow):
         layout.addWidget(self.render_area, 0, 2, 12, 1)
 
         central_widget.setLayout(layout)
+
+    def calculate(self):
+        b = int(self.b_entry.text())
+        h = int(self.h_entry.text())
+        # expo = self.expo_entry.text()
+        # fck = int(self.fck_entry.text())
+        # yc = float(self.yc_entry.text())
+        # fyk = int(self.fyk_entry.text())
+        # ys = float(self.ys_entry.text())
+        # Md = float(self.Md_entry.text())
+        # x_d = float(self.x_d_entry.text())
+
+        # beam = RectBeam(b, h, expo, fck, yc, fyk, ys, Md, x_d)
+
+        self.render_area.input_values["b"] = b
+        self.render_area.input_values["h"] = h
+
+        self.render_area.update()
+
 
 if __name__ == "__main__":
     app = QtW.QApplication([])
